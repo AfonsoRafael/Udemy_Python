b41eb5ac7aead161ac952d343cae2527be0aafd4
Afonso Rafael <programadoafonso@gmail.com>
2026-01-12 19:53:15 -0300

    Realizando revisao detalhada em um curso paralelo

---
+++ basico/001comentario.py
@@ -0,0 +1,7 @@
+# comentario em Python
+print("Olá, Mundo!")  # imprime uma mensagem na tela
+"""
+nao é executado, apenas um comentário mas o python interpreta como string de múltiplas linhas
+Este é um comentário de múltiplas linhas.
+docstring, o interpretador do Python lê esse texto
+"""
---
+++ basico/002print.py
@@ -0,0 +1,6 @@
+# sep define os separadores entre os valores
+# end define o que será colocado ao final da impressão
+print(12, 34, 1011, sep="", end='#')# sem espaço e com # no final
+print(56, 78, sep="", end='#')# sem espaço e com # no final
+print(56, 78, sep='-', end='\n')# padrao é espaço e quebra de linha
+print(9, 10, sep='-', end='\n')
