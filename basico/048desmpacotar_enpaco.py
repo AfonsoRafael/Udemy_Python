3b075305c02462a7c10ec00540a2e263216e5fa9
Afonso Rafael <programadoafonso@gmail.com>
2026-01-15 16:31:52 -0300

    Revisão

---
+++ basico/048desmpacotar_enpaco.py
@@ -0,0 +1,6 @@
+"""
+Introdução ao empacotamento e desempacotamento
+"""
+# _ como variavel significa que não sera utilizado
+_, _, nome, *resto = ['Maria', 'Helena', 'Luiz','Afonso','Carol']
+print(nome)
